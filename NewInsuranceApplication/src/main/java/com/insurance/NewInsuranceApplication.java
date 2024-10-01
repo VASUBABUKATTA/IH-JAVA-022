4f0acbe43adbfbfc9604b1c295beabb359068e72
VASUBABU KATTA <VASUBABUKATTA>
2024-10-01 16:39:27 +0530

    Adding Extra Exceptions handling Globally

---
+++ NewInsuranceApplication/src/main/java/com/insurance/NewInsuranceApplication.java
@@ -0,0 +1,31 @@
+package com.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@OpenAPIDefinition(
+	    info = @Info(
+	        title = "Property Insurance Application",
+	        version = "3.0",
+	        description = "API documentation for my project. Developed by Property Team."
+	    )
+	)
+public class NewInsuranceApplication extends SpringBootServletInitializer {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewInsuranceApplication.class, args);
+	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(NewInsuranceApplication.class);
+	    }
+
+}
